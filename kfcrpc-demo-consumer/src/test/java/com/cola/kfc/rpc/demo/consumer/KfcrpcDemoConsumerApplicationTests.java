68a7db3f09c630384f18675c6263db196772ddab
zjx <2442460982@qq.com>
2024-04-06 08:07:25 +0800

    test not need provider

--- kfcrpc-demo-consumer/src/test/java/com/cola/kfc/rpc/demo/consumer/KfcrpcDemoConsumerApplicationTests.java
+++ kfcrpc-demo-consumer/src/test/java/com/cola/kfc/rpc/demo/consumer/KfcrpcDemoConsumerApplicationTests.java
@@ -1,13 +1,33 @@
 package com.cola.kfc.rpc.demo.consumer;
 
+import com.cola.kfcrpc.demo.provider.KfcrpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class KfcrpcDemoConsumerApplicationTests {
 
+    static ApplicationContext context;
+
+    @BeforeAll
+    static void start(){
+         context = SpringApplication.run(KfcrpcDemoProviderApplication.class,
+                "server.port=8084");
+    }
+
     @Test
     void contextLoads() {
+        System.out.printf("====> aaaa");
+    }
+
+    @AfterAll
+    static void stop(){
+        SpringApplication.exit(context,()->1);
     }
 
 }
