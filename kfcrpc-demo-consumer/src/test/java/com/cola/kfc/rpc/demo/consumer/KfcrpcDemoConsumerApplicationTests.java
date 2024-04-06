792bad1cae4ef2e42f06ae6bf192464368fba13f
zjx <2442460982@qq.com>
2024-04-06 09:36:21 +0800

    add classfier slove find provider fail

--- kfcrpc-demo-consumer/src/test/java/com/cola/kfc/rpc/demo/consumer/KfcrpcDemoConsumerApplicationTests.java
+++ kfcrpc-demo-consumer/src/test/java/com/cola/kfc/rpc/demo/consumer/KfcrpcDemoConsumerApplicationTests.java
@@ -1,22 +1,22 @@
 package com.cola.kfc.rpc.demo.consumer;
-
 import com.cola.kfcrpc.demo.provider.KfcrpcDemoProviderApplication;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class KfcrpcDemoConsumerApplicationTests {
 
     static ApplicationContext context;
 
+    @SneakyThrows
     @BeforeAll
     static void start(){
-         context = SpringApplication.run(KfcrpcDemoProviderApplication.class,
+        context = SpringApplication.run(KfcrpcDemoProviderApplication.class,
                 "server.port=8084");
     }
 
